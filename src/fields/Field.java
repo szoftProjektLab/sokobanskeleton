38a2527c517822f654f519dc096836b33a241f9e
erikboros <boros.erik@gmail.com>
2018-03-15 19:59:29 +0100

    Field
    metódusok + kommentek

--- src/fields/Field.java
+++ src/fields/Field.java
@@ -1,25 +1,96 @@
 package fields;
 
 import enums.Direction;
+import game.Skeleton;
 import things.Thing;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Field {
 
-    private Direction neighbours;
+    private Map<Direction,Field> neighbours;
     protected Thing thing;
 
+    /**
+     * Konstruktor: neighbours map inicializálása
+     */
+    public Field(){
+        //Kell konstruktorba? Skeleton.getInstance.Return(this);
+        neighbours = new HashMap<Direction,Field>();
+    }
 
-    public void SetNeighbour(Direction d, Field f){}
+    /**
+     * Az adott irányba beállítja a szomszédos mezőt.
+     * @param d irány
+     * @param f szomszédnak szánt Field
+     */
+    public void SetNeighbour(Direction d, Field f){
+        //Kell setterbe? Skeleton.getInstance.Return(this);
+        neighbours.put(d, f);
+    }
 
-    public int Add(Thing t){ return 0;}
+    /**
+     * Egy tárgyat hozzácsatol az aktuális mezőhöz.
+     * @param t A csatolandó tárgy
+     * @return 0
+     */
+    public int Add(Thing t){
+        this.thing = t;
+        Skeleton.getInstance.Call(this, this, "Interact");
+        int tmp = Interact(t);
+        Skeleton.getInstance.Return(this);
+        return tmp;
+    }
 
-    public void Remove(){}
+    /**
+     * Az éppen rajta álló tárgyat eltávolítja a mezőről
+     */
+    public void Remove(){
+        this.thing = null;
+        Skeleton.getInstance.Return(this);
+    }
 
+    /**
+     * Virtuális függvény, itt nem csinál semmit
+     * @param t
+     * @return
+     */
     public int Interact(Thing t){ return 0;}
 
-    public int TryMove(Direction d){return 0;}
 
-    public int TryMove(Direction d, Thing t){return 0;}
+    /**
+     * A mezőn álló játékos ezzel jelzi helyváltoztatási szándékát.
+     * @param d A mozgás iránya
+     * @return
+     */
+    public int TryMove(Direction d){
+        //return neighbours.get(d).TryMove(d, this.thing);
+        Skeleton.getInstance.Call(this, neighbours.get(d), "TryMove");
+        int tmp = neighbours.get(d).TryMove(d, this.thing);
+
+        Skeleton.getInstance.Return(this);
+        return tmp;
+    }
+
+    /**
+     * A szomszédos mező a felőle érkező mozgás igényt ennek meghívásával jelzi.
+     * @param d A mozgás iránya
+     * @param t Az érkezni kívánó Thing
+     * @return
+     */
+    public int TryMove(Direction d, Thing t){
+        int tmp =0;
+        if (this.thing==null){
+            Skeleton.getInstance.Call(this, t, "AcceptMove");
+            tmp = t.AcceptMove(this);
+        } else{
+            Skeleton.getInstance.Call(this, t, "MakeCollision");
+            tmp = t.MakeCollision(d, this.thing);
+        }
+        Skeleton.getInstance.Return(this);
+        return tmp;
+    }
 
 
 }
