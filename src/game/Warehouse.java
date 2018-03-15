6b9bc0184837c6bc413e752d1d29c75722027165
IncredibleK <aron11111@gmail.com>
2018-03-15 16:29:33 +0100

    Warehouse váza elkészítve, nem teljes még

--- src/game/Warehouse.java
+++ src/game/Warehouse.java
@@ -1,4 +1,28 @@
 package game;
 
+
 public class Warehouse {
+
+    private String name;
+    private int colouredBoxCount;
+    private int playerCount;
+
+    public void StartingProcess(){}
+
+    public void CBDecrease() {
+        colouredBoxCount--;
+        if (colouredBoxCount == 0) {
+            Skeleton.Call();
+            Game.EndGame();
+        }
+        Skeleton.Return();
+    }
+    public void PDecrease(){
+        playerCount--;
+        if(playerCount==0) {
+            Skeleton.Call();
+            Game.EndGame();
+        }
+        Skeleton.Return();
+    }
 }
