4ac56c5c8480f15c0adf3689de2deeab0a1c14e5
IncredibleK <aron11111@gmail.com>
2018-03-15 17:05:16 +0100

    Warehouse függvények pontosítása, kommentelése

--- src/game/Warehouse.java
+++ src/game/Warehouse.java
@@ -1,28 +1,42 @@
 package game;
 
-
 public class Warehouse {
 
     private String name;
     private int colouredBoxCount;
     private int playerCount;
 
+
     public void StartingProcess(){}
 
     public void CBDecrease() {
+        //Színes dobozok száma csökkentése
         colouredBoxCount--;
+        //Skeleton singleton osztály lekérdezése és eltárolása
+        Skeleton skeleton = Skeleton.getInstance();
+        //Színes dobozok száma ellenőrzés
         if (colouredBoxCount == 0) {
-            Skeleton.Call();
-            Game.EndGame();
+            //Hívás előtt a skeleton kiíró függvényét meghívjuk
+            skeleton.getInstance().Call(this,skeleton,"EndGame");
+            //Véget ér a játék, meghívjuk a singleton Game osztálynak a függvényét
+            Game.getInstance().EndGame();
         }
-        Skeleton.Return();
+        //Visszatérünk, meghívjuk a visszatérési függvényt
+        skeleton.Return(this);
     }
     public void PDecrease(){
+        //Játékos dobozok száma csökkentése
         playerCount--;
-        if(playerCount==0) {
-            Skeleton.Call();
-            Game.EndGame();
+        //Skeleton singleton osztály lekérdezése és eltárolása
+        Skeleton skeleton = Skeleton.getInstance();
+        //Játékosok száma ellenőrzés
+        if(playerCount==1) {
+            //Hívás előtt a skeleton kiíró függvényét meghívjuk
+            skeleton.Call(this,skeleton,"EndGame");
+            //Véget ér a játék, meghívjuk a singleton Game osztálynak a függvényét
+            Game.getInstance().EndGame();
         }
-        Skeleton.Return();
+        //Visszatérünk, meghívjuk a visszatérési függvényt
+        skeleton.Return(this);
     }
 }
