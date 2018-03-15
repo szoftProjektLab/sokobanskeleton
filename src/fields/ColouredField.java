70fb56e62a6929fd46b72cf79376ef05ceacb59f
erikboros <boros.erik@gmail.com>
2018-03-15 20:31:18 +0100

    ColouredField
    -ColouredField.void InitColour()
    -private ColouredBox box; //thing==box nem lehet

--- src/fields/ColouredField.java
+++ src/fields/ColouredField.java
@@ -1,13 +1,34 @@
 package fields;
 
 import display.Colours;
+import game.Skeleton;
+import things.ColouredBox;
+import things.Thing;
 
-public class ColouredField {
+public class ColouredField extends Field{
 
-    private int Red;
-    private int Green;
-    private int Blue;
+    private Colours colour;
+    //private ColouredBox box; //thing==box nem lehet
+    private Thing box;
 
+    public void InitColour(){
+        //???
+    }
 
-    public Colours RandomColour(){return null;}
+    /**
+     * Ha színes láda kerül rá, ellenőrzi, hogy a hozzá tartozó áll e rajta,
+     * ha igen, pontot ad és kitörli.
+     * @param cb Rajta álló színes láda
+     * @return pont
+     */
+    public int Interact(ColouredBox cb){
+        int tmp=0;
+        if (thing==box){
+            tmp=1;
+            Skeleton.getInstance.Call(this, thing,"Die");
+            thing.Die();
+        }
+        Skeleton.getInstance.Return(this);
+        return tmp;
+    }
 }
