c0e974cfd0faed8eedf0ccddc0332cd64f585c8c
IncredibleK <aron11111@gmail.com>
2018-03-15 17:10:56 +0100

    Warehouse függvények pontosítása, kommentelése

--- src/game/Warehouse.java
+++ src/game/Warehouse.java
@@ -2,41 +2,43 @@ package game;
 
 public class Warehouse {
 
-    private String name;
+    //Színes dobozok száma
     private int colouredBoxCount;
+    //Játékosok száma
     private int playerCount;
 
-
+    //Kezdési folyamat
     public void StartingProcess(){}
 
     public void CBDecrease() {
         //Színes dobozok száma csökkentése
         colouredBoxCount--;
-        //Skeleton singleton osztály lekérdezése és eltárolása
-        Skeleton skeleton = Skeleton.getInstance();
         //Színes dobozok száma ellenőrzés
         if (colouredBoxCount == 0) {
+            //Játék referencia lekérdezése
+            Game game = Game.getInstance();
             //Hívás előtt a skeleton kiíró függvényét meghívjuk
-            skeleton.getInstance().Call(this,skeleton,"EndGame");
+            Skeleton.getInstance().Call(this,game,"EndGame");
             //Véget ér a játék, meghívjuk a singleton Game osztálynak a függvényét
-            Game.getInstance().EndGame();
+            game.EndGame();
         }
         //Visszatérünk, meghívjuk a visszatérési függvényt
-        skeleton.Return(this);
+        Skeleton.getInstance().Return(this);
     }
+
     public void PDecrease(){
         //Játékos dobozok száma csökkentése
         playerCount--;
-        //Skeleton singleton osztály lekérdezése és eltárolása
-        Skeleton skeleton = Skeleton.getInstance();
         //Játékosok száma ellenőrzés
         if(playerCount==1) {
+            //Játék referencia lekérdezése
+            Game game = Game.getInstance();
             //Hívás előtt a skeleton kiíró függvényét meghívjuk
-            skeleton.Call(this,skeleton,"EndGame");
+            Skeleton.getInstance().Call(this,game,"EndGame");
             //Véget ér a játék, meghívjuk a singleton Game osztálynak a függvényét
-            Game.getInstance().EndGame();
+            game.EndGame();
         }
         //Visszatérünk, meghívjuk a visszatérési függvényt
-        skeleton.Return(this);
+        Skeleton.getInstance().Return(this);
     }
 }
