f4c8c83e7c0250141dafd4fe802fb704429c333d
IncredibleK <aron11111@gmail.com>
2018-03-16 19:07:03 +0100

    Kisebb módosítások a kommentekben

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,4 +1,17 @@
 package game;
 
 public class Game {
+
+    private static Game game = new Game();
+    private Warehouse running;
+    private Game(){}
+
+    public static Game getInstance(){
+        return game;
+    }
+    public void StartGame(){}
+    public void setRunning(Warehouse r){running = r; }
+    public void EndGame(){
+        Skeleton.getInstance().Clear();
+    }
 }
